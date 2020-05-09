811afec37d9d640cdcf7730326e439511c75c7cf
frozenlib <frozenlib@users.noreply.github.com>
2020-05-09 18:19:39 +0900

    DangerousMutexFutureを別ファイルに分離

--- tokio-example/examples/mutex-drop-before-ready.rs
+++ tokio-example/examples/mutex-drop-before-ready.rs
@@ -1,9 +1,6 @@
 use std::task::Context;
 use std::{future::Future, pin::Pin, sync::Arc, task::Poll, thread::sleep, time::Duration};
-use tokio::{
-    sync::{Mutex, MutexGuard},
-    task::spawn,
-};
+use tokio::{sync::Mutex, task::spawn};
 
 // Mutex::lock()の戻り値のFutureを破棄すると、そのFutureを再開するためのWaker::wakeは呼び出されないという例
 
@@ -30,13 +27,9 @@ async fn main() {
         println!("lock succeeded");
     });
 
-    // wakeの前にFutureを破棄しない場合
-    let h4 = spawn(DangerousMutexFuture::new(m.clone()));
-
     h1.await.unwrap();
     h2.await.unwrap();
     h3.await.unwrap();
-    h4.await.unwrap();
 }
 
 struct BadMutexFuture(Arc<Mutex<()>>);
@@ -51,52 +44,6 @@ impl Future for BadMutexFuture {
             }
             Poll::Pending => {
                 println!("pending bad");
-                Poll::Pending
-            }
-        }
-    }
-}
-
-struct DangerousMutexFuture {
-    mutex: Arc<Mutex<()>>,
-
-    // 正しいライフタイムを表現できないため、仕方なくstaticライフタイムを使っている
-    // これがUBを引き起こさないか心配
-    fut: Option<Pin<Box<dyn Future<Output = MutexGuard<'static, ()>> + Send>>>,
-}
-
-impl Drop for DangerousMutexFuture {
-    fn drop(&mut self) {
-        // futはmutexを参照しているので、mutexより先にfutが解放されるとヤバい
-        self.fut.take();
-    }
-}
-
-impl DangerousMutexFuture {
-    fn new(mutex: Arc<Mutex<()>>) -> Self {
-        Self { mutex, fut: None }
-    }
-}
-
-impl Future for DangerousMutexFuture {
-    type Output = ();
-    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
-        let s = self.get_mut();
-        let fut = &mut s.fut;
-        if fut.is_none() {
-            let mutex = &s.mutex as &Mutex<()> as *const Mutex<()>;
-            unsafe {
-                *fut = Some(Box::pin((*mutex).lock()));
-            }
-        }
-        match fut.as_mut().unwrap().as_mut().poll(cx) {
-            Poll::Ready(_) => {
-                fut.take();
-                println!("ready dangerous");
-                Poll::Ready(())
-            }
-            Poll::Pending => {
-                println!("pending dangerous");
                 Poll::Pending
             }
         }
---
+++ tokio-example/examples/mutex-save-future-dangerous.rs
@@ -0,0 +1,70 @@
+use std::task::Context;
+use std::{future::Future, pin::Pin, sync::Arc, task::Poll, thread::sleep, time::Duration};
+use tokio::{
+    sync::{Mutex, MutexGuard},
+    task::spawn,
+};
+
+#[tokio::main]
+async fn main() {
+    let m = Arc::new(Mutex::new(()));
+
+    let m1 = m.clone();
+    let h1 = spawn(async move {
+        let _guard = m1.lock().await;
+        println!("sleep start");
+        sleep(Duration::from_secs(5));
+        println!("sleep end");
+    });
+    sleep(Duration::from_secs(1));
+
+    let h2 = spawn(DangerousMutexFuture::new(m.clone()));
+
+    h1.await.unwrap();
+    h2.await.unwrap();
+}
+struct DangerousMutexFuture {
+    mutex: Arc<Mutex<()>>,
+
+    // 正しいライフタイムを表現できないため、仕方なくstaticライフタイムを使っている
+    // これがUBを引き起こさないか心配
+    fut: Option<Pin<Box<dyn Future<Output = MutexGuard<'static, ()>> + Send>>>,
+}
+
+impl Drop for DangerousMutexFuture {
+    fn drop(&mut self) {
+        // futはmutexを参照しているので、mutexより先にfutが解放されるとヤバい
+        self.fut.take();
+    }
+}
+
+impl DangerousMutexFuture {
+    fn new(mutex: Arc<Mutex<()>>) -> Self {
+        Self { mutex, fut: None }
+    }
+}
+
+impl Future for DangerousMutexFuture {
+    type Output = ();
+    fn poll(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<Self::Output> {
+        let s = self.get_mut();
+        let fut = &mut s.fut;
+        if fut.is_none() {
+            let mutex = &s.mutex as &Mutex<()> as *const Mutex<()>;
+            unsafe {
+                *fut = Some(Box::pin((*mutex).lock()));
+            }
+        }
+        match fut.as_mut().unwrap().as_mut().poll(cx) {
+            Poll::Ready(_) => {
+                fut.take();
+                println!("ready");
+                Poll::Ready(())
+            }
+            Poll::Pending => {
+                println!("pending");
+                Poll::Pending
+            }
+        }
+    }
+}
